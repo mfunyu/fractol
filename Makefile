721e562d6c5469d30379eab2419506f5bf141889
mfunyu <mfunyu@student.42tokyo.jp>
2021-08-07 15:29:58 +0900

    [add] leak_check

--- Makefile
+++ Makefile
@@ -6,37 +6,43 @@
 #    By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/08/05 21:20:46 by mfunyu            #+#    #+#              #
-#    Updated: 2021/08/05 21:42:54 by mfunyu           ###   ########.fr        #
+#    Updated: 2021/08/07 15:27:17 by mfunyu           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME	:= fractol
-SRCS	:= color.c \
+
+SRCFILES:= color.c \
 		exit.c \
 		hooks.c \
 		julia.c \
-		main.c \
 		mandelbrot.c \
 		params.c \
 		puts.c \
 		utils.c \
 		zoom.c
+SRCS	:= $(SRCFILES) main.c
 SRCS	:= $(addprefix srcs/,$(SRCS))
 OBJS	:= $(SRCS:.c=.o)
+
+SRCS_LEAK := $(SRCFILES) main_leak_check.c
+SRCS_LEAK := $(addprefix srcs/,$(SRCS_LEAK))
+OBJS_LEAK	:= $(SRCS_LEAK:.c=.o)
+
 #DEPS	:= $(SRCS:.c=.d)
 
 LIBFT	:= libft
 LIBMLX	:= minilibx-linux
 LIBX_INC:= $(shell ./configure)
-INCLUDES:= -Iinclude -I$(LIBFT) -I$(LIBMLX) -I$(LIBX_INC)
-
-CC		:= gcc
-CFLAGS	:= -Wall -Wextra -Werror $(INCLUDES)
-
 LIBS	:= -L$(LIBFT) -lft \
 			-L$(LIBMLX) -lmlx_$(shell uname) \
 			-L$(LIBX_INC)/../lib -lXext -lX11 \
 			-lm
+
+INCLUDES:= -Iinclude -I$(LIBFT) -I$(LIBMLX) -I$(LIBX_INC)
+CC		:= gcc
+CFLAGS	:= -Wall -Wextra -Werror $(INCLUDES)
+
 
 .PHONY	: all clean fclean re
 
@@ -56,6 +62,12 @@ ifneq ($(shell echo ${LIBMLX}), $(shell ls | grep ${LIBMLX})))
 	git clone https://github.com/42Paris/minilibx-linux.git
 endif
 
+leak	: fclean $(LIBMLX) $(OBJS_LEAK)
+	make -C $(LIBFT)
+	make -C $(LIBMLX)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS_LEAK) $(LIBS)
+
+
 # compile .d files
 # %.o : %.c
 # 	$(CC) $(CFLAGS) -c -MMD -MF $(patsubst %.o,%.d,$@) -o $@ $<
@@ -65,7 +77,7 @@ clean	:
 ifeq ($(shell echo ${LIBMLX}), $(shell ls | grep ${LIBMLX})))
 	make clean -C $(LIBMLX)
 endif
-	$(RM) $(OBJS) $(DEPS)
+	$(RM) $(OBJS) $(OBJS_LEAK) $(DEPS)
 
 fclean	: clean
 	make fclean -C $(LIBFT)
---
+++ srcs/main_leak_check.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_leak_check.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/05 21:20:06 by mfunyu            #+#    #+#             */
+/*   Updated: 2021/08/07 15:12:39 by mfunyu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	end(void)__attribute__((destructor));
+
+void	end(void)
+{
+	system("leaks fractol");
+}
+
+void	init_t_mlx(t_mlx *mlx, char **av)
+{
+	mlx->mlx = mlx_init();
+	mlx->screen = set_screen_size(mlx, NULL);
+	mlx->window = mlx_new_window(mlx->mlx, mlx->screen, mlx->screen, av[1]);
+	if (!mlx->window)
+		error_exit("mlx window");
+}
+
+void	init_t_fractal(t_fractal *fractal, t_mlx *mlx)
+{
+	fractal->screen = mlx->screen;
+	fractal->type = set_fractal_type(NULL);
+	fractal->x_min = MIN;
+	fractal->y_min = MIN;
+	fractal->size = SIZE;
+	fractal->c_real = C_REAL;
+	fractal->c_imag = C_IMAG;
+	fractal->color = 0;
+	fractal->resolution = set_resolution(NULL);
+	fractal->loop = calc_loop(fractal);
+	mlx->fractal = fractal;
+}
+
+void	start_mlx_loop(t_mlx *mlx)
+{
+	mlx_key_hook(mlx->window, key_hook, mlx);
+	mlx_mouse_hook(mlx->window, mouse_hook, mlx);
+	mlx_hook(mlx->window, DestroyNotify, StructureNotifyMask, just_exit, NULL);
+	mlx_hook(mlx->window, FocusIn, FocusChangeMask, focus_change, mlx);
+	mlx_hook(mlx->window, KeyPress, KeyPressMask, key_press, mlx);
+	mlx_loop(mlx->mlx);
+}
+
+int	main(int ac, char **av)
+{
+	t_mlx		mlx;
+	t_fractal	frac;
+
+	check_params(ac, av);
+	init_t_mlx(&mlx, av);
+	init_t_fractal(&frac, &mlx);
+	put_fractal(&mlx);
+	start_mlx_loop(&mlx);
+	return (0);
+}
