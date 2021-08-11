6b7ea88118eaea442966f2b07edb9158ae4ef482
mfunyu <mfunyu@student.42tokyo.jp>
2021-08-11 21:43:37 +0900

    [fix] y axis, add comments

--- srcs/julia.c
+++ srcs/julia.c
@@ -6,11 +6,21 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 21:20:02 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/08/05 21:20:02 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/08/11 21:42:41 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+/*
+** x / frac->screen :
+** calc percentage of the pixel length (px_0 to px_x)
+** in relation to the total screen pixel size
+** * frac->size :
+** convert the pixel length to a size of the fractal's complex plane
+** frac->x_min + :
+** add a value of the px_0 coordinate to the px_x length
+*/
 
 int	julia_set(double x, double y, t_fractal *frac)
 {
@@ -20,7 +30,7 @@ int	julia_set(double x, double y, t_fractal *frac)
 	int		n;
 
 	zx = frac->x_min + x / frac->screen * frac->size;
-	zy = frac->y_min + y / frac->screen * frac->size;
+	zy = frac->y_min + (1 - y / frac->screen) * frac->size;
 	n = 1;
 	while (n <= frac->loop)
 	{
--- srcs/mandelbrot.c
+++ srcs/mandelbrot.c
@@ -6,11 +6,21 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 21:20:11 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/08/05 21:20:11 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/08/11 21:42:48 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+/*
+** x / frac->screen :
+** calc percentage of the pixel length (px_0 to px_x)
+** in relation to the total screen pixel size
+** * frac->size :
+** convert the pixel length to a size of the fractal's complex plane
+** frac->x_min + :
+** add a value of the px_0 coordinate to the px_x length
+*/
 
 int	mandelbrot_set(double x, double y, t_fractal *frac)
 {
@@ -20,7 +30,7 @@ int	mandelbrot_set(double x, double y, t_fractal *frac)
 	int		n;
 
 	x = frac->x_min + x / frac->screen * frac->size;
-	y = frac->y_min + y / frac->screen * frac->size;
+	y = frac->y_min + (1 - y / frac->screen) * frac->size;
 	zx = 0;
 	zy = 0;
 	n = 1;
